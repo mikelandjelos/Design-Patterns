418f321850a9cb76d01e94584b1d36578208a7e2
unknown <mihajlo.madic@gmail.com>
2022-07-03 00:14:26 +0200

    strategy pattern

---
+++ 2_SingletonPatternExample/LoadBalancer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternMyExample
+{
+    internal class LoadBalancer
+    {
+        #region Attributes
+
+        private List<string> servers;
+
+        private static LoadBalancer instance;
+        private static readonly object syncLock = new object();
+
+        #endregion // !Attributes
+
+        #region Singleton Util
+        private LoadBalancer()
+            => servers = new List<string>();
+
+        public static LoadBalancer Instance
+        {
+            get
+            { 
+                if (instance == null)
+                    lock (syncLock)
+                        if (instance == null)
+                            instance = new LoadBalancer();
+                return instance;
+            }
+        }
+
+        #endregion // !Singleton Util
+
+        #region Business Logic
+        public string RandomServer
+        {
+            get 
+            {
+                Random rand = new Random();
+                return servers[rand.Next() % servers.Count];
+            }
+        }
+
+        public string NewServer
+        { set => servers.Add(value); }
+
+        public void Clear() => servers.Clear();
+        #endregion // !Business Logic
+
+    }
+}
---
+++ 2_SingletonPatternExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternMyExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LoadBalancer.Instance.NewServer = "Server1";
+            LoadBalancer.Instance.NewServer = "Server2";
+            LoadBalancer.Instance.NewServer = "Server3";
+            LoadBalancer.Instance.NewServer = "Server4";
+
+            LoadBalancer instance1 = LoadBalancer.Instance;
+            LoadBalancer instance2 = LoadBalancer.Instance;
+
+            Console.WriteLine(LoadBalancer.Instance.RandomServer);
+            Console.WriteLine((instance1 == instance2) 
+                ? "same instance" : "different instances");
+
+        }
+    }
+}
